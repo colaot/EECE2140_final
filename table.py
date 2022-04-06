dd9ebce6459f2b3151337f03b590cf84d6920a1b
JustinBahr305 <bahr.j@northeastern.edu>
2022-04-06 14:06:58 -0400

    Table Class

--- table.py
+++ table.py
@@ -1,11 +1,23 @@
 from card import Card
 
 class Table:
-  def __init(self, num_players, deck):
-    self.num_players = num_players
+  def __init(self, players, deck, playerHand = []):
+    self.players = players
+    self.num_players = len(players)
     self.deck = deck
+    self.player_hand = player_hand
     
   def get_num_players(self):
     return self.num_players
+
+  def deal(self):
+    for i in [1, 2]:
+      for i in range(num_players):
+        if (players[i] == None)
+          self.deck.pop()
+        else:
+          self.playerHand.append(self.deck.pop())
+          
+
   
   
