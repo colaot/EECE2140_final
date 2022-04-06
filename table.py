33fb29c1f906336bce7d65fd79d33866f7ed0e88
JustinBahr305 <103138594+JustinBahr305@users.noreply.github.com>
2022-04-06 13:42:35 -0400

    Update table.py

--- table.py
+++ table.py
@@ -1,8 +1,9 @@
 from card import Card
 
 class Table:
-  def __init(self, num_players):
+  def __init(self, num_players, deck):
     self.num_players = num_players
+    self.deck = deck
     
   def get_num_players(self):
     return self.num_players
